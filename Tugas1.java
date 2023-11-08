185fbee18aaba17fa44b832fe16d62d328151bba
saarisdi <riyadi256789@gmail.com>
2023-11-08 08:51:48 +0700

    Mengubah isi class sesuai dengan modul

---
+++ Tugas1.java
@@ -0,0 +1,11 @@
+public class Tugas1 {
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+    try{
+System.out.println(3/0);
+} catch(Exception e) {
+        }
+    }
+}
