cc7f5c2c92818198fc396d7dd3bb3a48cf448add
saarisdi <riyadi256789@gmail.com>
2023-11-08 08:46:16 +0700

    Mengubah isi dari masing - masing class sesuai perintah modul

--- Tugas1.java
+++ Tugas1.java
@@ -3,6 +3,9 @@ public class Tugas1 {
 * @param args the command line arguments
 */
 public static void main(String[] args) {
+    try{
 System.out.println(3/0);
-}
+} catch(Exception e) {
+        }
+    }
 }
